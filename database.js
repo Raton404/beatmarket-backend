ebcf81df31279706b66d8b48e83a9b2a19ef096c
Raton404 <theslimbaki@gmail.com>
2024-11-29 14:57:45 -0300

    Add: Root route endpoint

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 22774,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
